a13e1e7e871e72e83b72d32ffb1207b606f8f1dd
mrb <michaelrbernstein@gmail.com>
2012-06-02 23:55:16 -0400

    Add to README, add LICENSE, add data route

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 ## Shoebox
+
+Shoebox is a proof of concept Go project that ties together `pat`, a route handler, `noeq`, a meaningful UUID generator, and `riakpbc`, a Riak Protocol Buffers client. It currently supports two routes:
+
+```go
+func GetId(w http.ResponseWriter, req *http.Request)
+  // Get a new ID
+  // [GET] /id/new
+
+func PostData(w http.ResponseWriter, req *http.Request)
+  // Post data to Riak and get a Unique ID
+  // [POST] /data
+  // Body: JSON data to store
+```
+
+### Requirements
+
+#### A running Riak cluster
+
+With a protocol buffer port listening on port 8087.
+
+#### A running noeqd instance
+
+Listening on port 4444.
+
+### Running
+
+`go build && ./shoebox`
+
+### Credits
+
+riakpbc is (c) Michael R. Bernstein, 2012
+
+### License
+
+shoebox is distributed under the MIT License, see `LICENSE` file for details.
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -14,33 +15,63 @@ import (
 var (
 	riakc *riakpbc.Conn
 	noeqc *noeq.Client
+	err   error
 )
 
 func GetId(w http.ResponseWriter, req *http.Request) {
 	id, _ := noeqc.GenOne()
 	stringId := fmt.Sprintf("%d", id)
+
+	log.Print(" [GET] {id: ", stringId, "}")
+
+	io.WriteString(w, stringId)
+}
+
+func PostData(w http.ResponseWriter, req *http.Request) {
+	body, _ := ioutil.ReadAll(req.Body)
+	id, _ := noeqc.GenOne()
+
+	stringId := fmt.Sprintf("%d", id)
+	stringBody := fmt.Sprintf("%s", body)
+
+	_, err := riakc.StoreObject("data", stringId, stringBody)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	log.Print("[POST] {id: ", stringId, "}, {body: ", stringBody, "}")
+
 	io.WriteString(w, stringId)
 }
 
 func main() {
-	riakc, err := riakpbc.Dial("127.0.0.1:8087")
+	riakc, err = riakpbc.New("127.0.0.1:8087", 1e8, 1e8)
 	if err != nil {
 		log.Print(err)
+		return
 	}
-
-	log.Print(riakc)
 
 	noeqc, err = noeq.New("", "127.0.0.1:4444")
 	if err != nil {
 		log.Print(err)
+		return
 	}
 
-	log.Print(noeqc)
+	err = riakc.Dial()
+	if err != nil {
+		log.Print(err)
+		return
+	}
 
 	m := pat.New()
 	m.Get("/id/new", http.HandlerFunc(GetId))
+	m.Post("/data", http.HandlerFunc(PostData))
 
 	http.Handle("/", m)
+
+	log.Print("[INFO] ...starting your shoebox....")
+
 	err = http.ListenAndServe(":12345", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
